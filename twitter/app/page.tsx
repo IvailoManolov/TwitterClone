364d9b3eb7986d23901d6e56f9c8c20448f7ccf7
IvailoManolov <ivomoreras@abv.bg>
2024-01-06 21:42:46 +0200

    --tailwind configurator

--- twitter/app/page.tsx
+++ twitter/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="text-3xl text-blue-500">
+      hello
     </main>
   )
 }
